57518bfb72fc4919e67538385bb22cbd21ba2d9b
andreamartinez16c <andrea.martinez16.c@gmail.com>
2023-11-17 13:49:01 +0100

    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { Prueba1Component } from './components/prueba1/prueba1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    Prueba1Component
   ],
   imports: [
     BrowserModule
